37595f5848f1d75edf9e22bf5b1a043856793138
Torrie Real <torijames1997@gmail.com>
2022-09-19 20:01:57 -0500

    -m "HEre's where the group got to before review"

--- src/org/launchcode/studio7/CD.java
+++ src/org/launchcode/studio7/CD.java
@@ -1,8 +1,26 @@
 package org.launchcode.studio7;
 
-public class CD {
+import java.util.ArrayList;
 
+public class CD extends BaseDisc implements OpticalDisc {
+
+    public CD(String name, int capacity, ArrayList<String> contents, String discType){
+        super(name, capacity, contents, discType);
+    }
     // TODO: Implement your custom interface.
+    @Override
+    public void spinDisc(){
+        System.out.println("A CD spins at a rate of 200 - 500 rpm.");
+    };
+
+    @Override
+    public void readData(){
+        System.out.println("CD is now playing");
+    }
+
+    @Override
+    public void storeData(){
+        System.out.println("CD data is stored");
 
     // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
     //  need to be declared separately.
